f7d4c13b6e10a3b7d03b5330bb2ea2dcc62b8b67
ungguljayabeton <ungguljayabeton@outlook.com>
2024-03-28 15:46:06 +0700

    Update

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,7 +2,7 @@ import { sql } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const users = sqliteTable('users', {
-  id: text('id').primaryKey(),
+  id: integer('id').primaryKey(),
   createdAt: text('created_at')
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
